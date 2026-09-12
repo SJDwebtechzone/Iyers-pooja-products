1d0934851bdb34dc206181007a4fe5b7e19b763b
unknown <santhosh.u2653@gmail.com>
2026-09-12 11:59:24 +0530

    remove unwanted frontend folder

--- frontend/frontend/.next/dev/static/chunks/_18x7jxu._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__1wcghiq._.css","static/chunks/node_modules_07m6cug._.js","static/chunks/app_components_1p0tp5w._.js"],
-    source: "entry"
-});
